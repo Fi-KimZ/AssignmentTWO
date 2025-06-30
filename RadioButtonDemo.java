af7d245b98942c8693d86977f4a8d14cd58970ca
Fi-KimZ <filexkim06@gmail.com>
2025-06-30 20:16:18 +0300

    Update RadioButtonDemo.java

--- RadioButtonDemo.java
+++ RadioButtonDemo.java
@@ -10,7 +10,7 @@ public class RadioButtonDemo extends JFrame implements ActionListener {
 
       public RadioButtonDemo() {
         setTitle("RadioButtonDemo");
-        setSize(800, 600);
+        setSize(400, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
